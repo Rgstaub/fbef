539631915adfe8351da5942922a2ce905aa213c6
Rgstaub <rgstaub@gmail.com>
2017-10-24 14:11:00 -0400

    added a test form

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import DonateButton from "./components/DonateButton";
 import NavbarHeader from "./components/NavbarHeader";
 import theme from './assets/react-toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import InputTest from './components/InputTest'
 import "./assets/react-toolbox/reset.css"
 import "./assets/react-toolbox/theme.css"
 
@@ -21,6 +22,7 @@ const App = () =>
   <Router>
     <div>
       <ThemeProvider theme={theme}><NavbarHeader /></ThemeProvider>
+      
       <Splash />
       <Demo1 />
       <Demo3 />
@@ -29,6 +31,7 @@ const App = () =>
       <Demo5 />
       <Demo6 />
       <Demo7 />
+      <ThemeProvider theme={theme}><InputTest /></ThemeProvider>
       {/* <Switch>
         <Route exact path="/" component={} />
       </Switch> */}
