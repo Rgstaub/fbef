5e0ca8715deb2087403d3d5d0aecf46a4d33bac5
adurha10 <alamadu82@gmail.com>
2017-10-23 21:44:04 -0400

    fixed css typo on splash

--- client/src/components/Splash/Splash.js
+++ client/src/components/Splash/Splash.js
@@ -4,7 +4,7 @@ import logo from "./logoWhite.png";
 
 const Splash = () =>
   <div style={{ height: 600, background: `linear-gradient(rgba(48,63,159, 0.5), rgba(48,63,159, 0.5)), url(${background})`, backgroundSize: "cover", margin: 0}}>
-    <img style={{position: "absolute", top: "50%", left: "50%"}}src={logo}></img>
+    <img style={{position: "absolute", top: "50%", left: "50%", height: "100px", width:"520px", marginLeft: "-260px", marginTop: "-50px"}}src={logo}></img>
   </div>;
 
 export default Splash;
