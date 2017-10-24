d69e2a022b14aaeffde9f62dc11dfd7647e38a49
adurha10 <alamadu82@gmail.com>
2017-10-23 21:31:14 -0400

    updated splash component, added styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Demo1 from "./pages/Demo1";
 import Demo2 from "./pages/Demo2";
 import Demo3 from "./pages/Demo3";
+import Splash from "./components/Splash"
 import DonateButton from "./components/DonateButton";
 import NavbarHeader from "./components/NavbarHeader";
 import theme from './assets/react-toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import "./assets/react-toolbox/reset.css"
 import "./assets/react-toolbox/theme.css"
 
 
@@ -15,7 +17,7 @@ const App = () =>
   <Router>
     <div>
       <ThemeProvider theme={theme}><NavbarHeader /></ThemeProvider>
-      <DonateButton />
+      <Splash />
       <Demo1 />
       <Demo3 />
       <Demo2 />
