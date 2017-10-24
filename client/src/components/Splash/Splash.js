10715f17ec9ba79432293ba064206cdcf2e539f3
adurha10 <alamadu82@gmail.com>
2017-10-23 21:46:12 -0400

    made splash overlay darker

--- client/src/components/Splash/Splash.js
+++ client/src/components/Splash/Splash.js
@@ -3,7 +3,7 @@ import background from "./group.JPG";
 import logo from "./logoWhite.png";
 
 const Splash = () =>
-  <div style={{ height: 600, background: `linear-gradient(rgba(48,63,159, 0.5), rgba(48,63,159, 0.5)), url(${background})`, backgroundSize: "cover", margin: 0}}>
+  <div style={{ height: 600, background: `linear-gradient(rgba(48,63,159, 0.75), rgba(48,63,159, 0.75)), url(${background})`, backgroundSize: "cover", margin: 0}}>
     <img style={{position: "absolute", top: "50%", left: "50%", height: "100px", width:"520px", marginLeft: "-260px", marginTop: "-50px"}}src={logo}></img>
   </div>;
 
